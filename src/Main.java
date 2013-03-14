56411921194a1087f6128c190cf4c7c737726197
Hannes Honkasaari <hayn0seh@gmail.com>
2013-03-14 18:54:14 +0200

    hopefully this will work
    im getting tired with this shit not working

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+	    int x = 0;
+        boolean working = true;
+
+        if (working && x == 0){
+            System.out.println("WORKINGUUUUUUU");
+        }
+        else {
+            System.out.println("NOT WORKINGUUUUUUU!");
+        }
+
+    }
+}
